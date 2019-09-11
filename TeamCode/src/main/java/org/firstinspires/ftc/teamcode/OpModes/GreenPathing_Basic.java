481263fd84212d7995bd4fa0fda53c974728ff24
shriya3ganesan <43021031+shriya3ganesan@users.noreply.github.com>
2019-09-11 16:54:03 -0500

    Added basic Green Pathing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/GreenPathing_Basic.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Hardware.DriveTrain;
+import org.firstinspires.ftc.teamcode.Hardware.Intake;
+import org.firstinspires.ftc.teamcode.Hardware.Sensors;
+
+@Autonomous(name ="Basic Blue Green Pathing", group="Auto Basic")
+public class GreenPathing_Basic extends LinearOpMode {
+
+    private double driveSpeed = 0.6;
+    DriveTrain driveTrain = new DriveTrain();
+    Sensors sensors = new Sensors();
+    Intake intake = new Intake();
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+
+        driveTrain.initDriveTrain(this);
+        driveTrain.resetEncoders();
+        waitForStart();
+
+        driveTrain.encoderDrive(driveSpeed,  96,  96, 96, 96, 4.0);
+        driveTrain.encoderDrive(driveSpeed, -48, -48, -48, -48, 3.0);
+        driveTrain.encoderDrive(driveSpeed,144, -144, -144, 144, 5.0);
+        driveTrain.encoderDrive(driveSpeed,-96, 96, 96, -96, 5.0);
+
+        sleep(1000);
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+    }
+
+}
