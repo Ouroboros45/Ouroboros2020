10149bbff7f794f3e346038256d5a85656555779
nirmiller <52181530+nirmiller@users.noreply.github.com>
2019-09-15 20:51:08 -0500

    Set up TeleOp intake and set up variables for Cruise Control Foundation Moving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Intake.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Hardware;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Intake {
@@ -17,27 +18,74 @@ public class Intake {
 
     private LinearOpMode opMode;
 
-    public boolean initIntake(LinearOpMode opMode)
+    private static final double PICKUP = 1;
+    private static final double IDLE = 0;
+
+    public boolean initIntake(OpMode opMode)
     {
-        this.opMode = opMode;
+        this.opMode = (LinearOpMode) opMode;
         try
         {
             rightSide = opMode.hardwareMap.dcMotor.get("Right Intake");
             leftSide = opMode.hardwareMap.dcMotor.get("Left Intake");
+
             opMode.telemetry.addData("Success", "Intake Initialized");
             opMode.telemetry.update();
+
         } catch (Exception e)
         {
             opMode.telemetry.addData("Failed", "Failed to Map Motors");
             opMode.telemetry.update();
+
             return false;
         }
+
+        rightSide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        leftSide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
         return true;
     }
 
     //is to be called in OpMode
-    public void compliantIntake()
+    //once activated moves toward block and activates compliant wheels
+    //blockFound set by vision
+    public void compliantIntake_Auto(boolean blockFound, double x_distance, double y_distance)
     {
+        if(blockFound)
+        {
+
+            //activate intake
+            rightSide.setPower(PICKUP);
+            leftSide.setPower(PICKUP);
+
+            //move toward block (x_distance, y_distance) - using vision
+            //uses encoder drive
+
+            opMode.telemetry.addData("Active", "Picking Up Block");
+            opMode.telemetry.update();
+
+
+        }else{
+            rightSide.setPower(IDLE);
+            leftSide.setPower(IDLE);
+        }
         //find out intake mechanism
+    }
+
+    public void compliantIntake_TeleOp()
+    {
+        if(opMode.gamepad1.x) //set gamepade button to x, could change
+        {
+            rightSide.setPower(PICKUP);
+            leftSide.setPower(PICKUP);
+
+            opMode.telemetry.addData("Active", "Intake Running");
+            opMode.telemetry.update();
+        }else
+        {
+            rightSide.setPower(IDLE);
+            leftSide.setPower(IDLE);
+        }
+
     }
 }
